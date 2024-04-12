b04853a990ba0734b56bda94d9a2d491be4fc928
Sergey <sgm3088@ya.ru>
2024-04-12 16:45:11 +0300

    v3

--- kivy_test.py
+++ kivy_test.py
@@ -1,15 +1,14 @@
 
 
 from kivymd.app import MDApp
-from kivymd.uix.screen import MDScreen
-from kivymd.uix.button import MDFlatButton, MDTextButton
 from kivy.lang import Builder
 
 
 class MainApp(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Dark"
-        self.theme_cls.primary_palette = "Orange"
+
+        self.theme_cls.primary_palette = "Green"
         screen = Builder.load_file('my.kv')
 
 
